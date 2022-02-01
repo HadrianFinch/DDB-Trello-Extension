6a40fd48a8cf7f32d0f1a888e97feba13136795c
Hadrian Finch <hadrianfinch@outlook.com>
2022-01-31 21:49:20 -0800

    add spellcard class

--- js/spells.js
+++ js/spells.js
@@ -1,5 +1,17 @@
 var t = window.TrelloPowerUp.iframe();
 
+const getTitle = (url) => 
+{  
+    return fetch(`https://crossorigin.me/${url}`)
+    .then((response) => response.text())
+    .then((html) => 
+    {
+        const doc = new DOMParser().parseFromString(html, "text/html");
+        const title = doc.querySelectorAll('title')[0];
+        return title.innerText;
+    });
+};
+
 // you can access arguments passed to your iframe like so
 var arg = t.arg('arg');
 
@@ -17,7 +29,19 @@ t.render(function ()
         .then(function (spellsAttachments) 
         {
             var urls = spellsAttachments.map(function (a) { return a.url; });
-            document.getElementById('urls').textContent = urls.join(', ');
+
+            for (let i = 0; i < urls.length; i++) 
+            {
+                var url = urls[i];
+                var container = document.querySelector('#content');
+
+                var div = document.createElement('div');
+                div.classList.add("spellCard");
+                container.appendChild(div);
+                
+                var spellNameElm = document.createElement('h1');
+                spellNameElm.innerHTML = getTitle(url);
+            }
         })
         .then(function () 
         {
